dac683025a1f82935c2f71f05c5002bf8a08c0bb
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2022-10-27 08:09:52 -0600

    V13 Bump (#5)
    * v13 bump
    
    Signed-off-by: Jordan Rash <15827604+jordan-rash@users.noreply.github.com>

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,14 +2,14 @@
 $ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url64      = 'https://github.com/wasmCloud/wash/releases/download/v0.12.0/wash.exe'
+$url64      = 'https://github.com/wasmCloud/wash/releases/download/v0.13.0/wash.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileFullPath  = "$toolsDir\wash.exe"
   url64bit      = $url64
 
-  checksum64    = '770E564990B075CF714E000559290BBF9F774C7C6360261B4499C74F2C648641'
+  checksum64    = 'B8BACFBFDBAA84AA35BE16A7FB0CAC911FA28878CD32C5CB93FC4DE6A3A2650F'
   checksumType64= 'sha256'
 }
 

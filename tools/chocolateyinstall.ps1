643c0f88bcc473ccfe12de4edf60f7bf26670393
Connor Smith <connor.smith.256@gmail.com>
2023-03-16 16:58:56 -0600

    update wash to v0.16.2
    Signed-off-by: Connor Smith <connor.smith.256@gmail.com>

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,14 +2,14 @@
 $ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url64      = 'https://github.com/wasmCloud/wash/releases/download/v0.16.1/wash.exe'
+$url64      = 'https://github.com/wasmCloud/wash/releases/download/v0.16.2/wash.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileFullPath  = "$toolsDir\wash.exe"
   url64bit      = $url64
 
-  checksum64    = '944D58473B48946D1983F8778954AF38C2840AD9B855AE54DA0B527A90237FC0'
+  checksum64    = '14151F20B18D441C050405245FB8E52724BEF13FD3CB1BDE9860DBB545BAD3A9'
   checksumType64= 'sha256'
 }
 

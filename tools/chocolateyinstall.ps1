8a543d27fa5dad4afa5f3c8a296de22d5179c366
Brooks Townsend <brooks@cosmonic.com>
2023-04-21 12:30:54 -0400

    bumped to v0.17.2 while we're at it
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,14 +2,14 @@
 $ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url64      = 'https://github.com/wasmCloud/wash/releases/download/v0.17.1/wash.exe'
+$url64      = 'https://github.com/wasmCloud/wash/releases/download/v0.17.2/wash.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileFullPath  = "$toolsDir\wash.exe"
   url64bit      = $url64
 
-  checksum64    = '8B2A883FE205FB6B6D43ECE54A62FA3286BC84DFE43DF04E606CEACF1614B885'
+  checksum64    = '71F59137B114698628FD1DB0848B43650A373814EB3F5902ABDD5227C1C5360B'
   checksumType64= 'sha256'
 }
 

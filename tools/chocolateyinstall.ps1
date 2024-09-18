0599bc26590be494f3eceb9d6fdae1c1e48e4f8f
brooksmtownsend <12040685+brooksmtownsend@users.noreply.github.com>
2024-09-18 14:24:09 +0000

    bump wash to v0.33.0
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,14 +2,14 @@
 $ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url64      = 'https://github.com/wasmCloud/wasmCloud/releases/download/wash-cli-v0.32.1/wash-x86_64-pc-windows-msvc.exe'
+$url64      = 'https://github.com/wasmCloud/wasmCloud/releases/download/wash-cli-v0.33.0/wash-x86_64-pc-windows-msvc.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileFullPath  = "$toolsDir\wash.exe"
   url64bit      = $url64
 
-  checksum64    = '3FE3BADEB52D96AD002A20892D1CB83C9D1885713CF31249EF781A310F97568D'
+  checksum64    = 'B9F85B3C61D7C38BB0FDE8905FC95EDD1C91370C08DC3CFBE858E78DA3D0A576'
   checksumType64= 'sha256'
 }
 

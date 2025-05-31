6bacea3ab13d6d30f2109a2ce79102f8a97ef083
Victor Adossi <123968127+vados-cosmonic@users.noreply.github.com>
2025-05-31 10:16:53 +0900

    fix: checksum for windows wash release
    Signed-off-by: Victor Adossi <123968127+vados-cosmonic@users.noreply.github.com>

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -9,7 +9,7 @@ $packageArgs = @{
   fileFullPath  = "$toolsDir\wash.exe"
   url64bit      = $url64
 
-  checksum64    = '0019DFC4B32D63C1392AA264AED2253C1E0C2FB09216F8E2CC269BBFB8BB49B5'
+  checksum64    = '2E95AC1C7BD309C3A29B63ADEF59918A90AB90BAFC26F80117F900112A39A29F'
   checksumType64= 'sha256'
 }
 
